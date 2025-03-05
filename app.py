760383797fb6726bb314507c8d2373a7a2cbc648
Izzatul Najwa <ismail.izzatulnajwa@gmail.com>
2025-03-05 14:53:57 +0800

    improve the visualisation

--- app.py
+++ app.py
@@ -2,11 +2,16 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 
 
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, callback, Output, Input
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 
-app = Dash()
+app = Dash(
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"}
+    ])
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
@@ -19,20 +24,65 @@ fig = px.line(sales_data, x='date', y='sales', title='Total Sales of Pink Morsel
                   "date": "Date"
               })
 
-app.layout = html.Div(children=[
-    html.H1(children='Data visualisation on the sales of the Pink Morsels'),
+radio_button = dcc.RadioItems([
+    {'label': 'East', 'value': 'east'},
+    {'label': 'North', 'value': 'north'},
+    {'label': 'West', 'value': 'west'},
+    {'label': 'South', 'value': 'south'},
+    {'label': 'All', 'value': 'all'},
+], 
+'all', 
+id="radio_buttons",
+className="d-flex justify-content-evenly"
+)
 
+
+app.layout = html.Div(children=[
+    html.H1(children='Data visualisation on the sales of the Pink Morsels', className="m-auto"),
     
-    dcc.Graph(
-        id='sales-chart',
-        figure=fig
+    html.Div(
+      id="line_graph"  
     ),
     
-    html.Div(children='''
-        After the increase of Pink Morsel's price one 15th January 2021, the sales had increased.
-    '''),
+    html.Div([
+        html.Label(children="Filter sales by region:"),
+        radio_button 
+    ], className="p-1",
+    style={
+        'border': '1px solid grey',
+        'border-radius': '10px'
+        }),
+    
+    
+    html.Div([
+        html.H3('Conclusion'),
+        html.P("After the increase of Pink Morsel's price one 15th January 2021, the sales had increased.")
+    ], className="my-3"),
 
-])
+], className="container py-2")
+
+@callback (
+    Output(component_id='line_graph', component_property='children'),
+    Input(component_id='radio_buttons', component_property='value')
+)
+def update_line_graph(input_value):
+    if input_value != 'all':
+        filtered_data = sales_data[sales_data['region'] == input_value] 
+    else:
+        filtered_data = sales_data
+        
+    filtered_fig = px.line(filtered_data, x='date', y='sales', title=f'Total Sales of Pink Morsels by Date: {input_value.capitalize()} region',
+              labels={
+                  "sales": "Total Sales ($)",
+                  "date": "Date"
+              })
+    
+    return dcc.Graph(
+        id='filtered-sales-chart',
+        figure=filtered_fig,
+        style={'width': '100%'}
+    )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
